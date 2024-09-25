a919ae3f21456f4346c49d5514a15beabd4cf482
Anoushka Singh <anshka42@gmail.com>
2024-09-25 09:37:03 +0530

    initial commit

---
+++ src/books.js
@@ -0,0 +1,31 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+import img4 from "./images/book4.jpg";
+
+export const books = [
+  {
+    author: "Kristin Hannah",
+    title: "The Women: A Novel",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Sarah J. Maas",
+    title: "A Court of Thorns and Roses ",
+    img: img3,
+    id: 3,
+  },
+  {
+    author: "Frank Herbert",
+    title: "Dune",
+    img: img4,
+    id: 4,
+  },
+];
